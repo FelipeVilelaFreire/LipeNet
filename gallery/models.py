c27f2fd3c3b43009ce2694e2a8b79089d70b28c9
Felipe <felipe.vilela.freire@gmail.com>
2025-07-23 19:19:32 -0300

    feat: Cria modelo Photo e o registra no admin

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Photos(models.Model):
+    text = models.TextField()
+    image = models.ImageField(upload_to='photos/')
+    # TextField é para textos longos. blank=True e null=True significam que este campo é opcional.
+    caption = models.TextField(blank=True, null=True)
+    # Coluna para registrar a data e hora do upload.
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
 # Create your models here.
