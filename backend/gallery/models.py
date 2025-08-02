ce1605f5ac3b0f2c3ab443fee40b141d81fafc85
Felipe <felipe.vilela.freire@gmail.com>
2025-08-02 15:26:00 -0300

    feat: Adiciona campo 'owner' ao modelo Photo

--- backend/gallery/models.py
+++ backend/gallery/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Tag(models.Model):
@@ -23,6 +24,7 @@ class Person(models.Model):
         return self.name
 
 class Photo(models.Model):
+    owner = models.ForeignKey(User, related_name='photos', on_delete=models.CASCADE)
     text = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='photos/')
     # TextField é para textos longos. blank=True e null=True significam que este campo é opcional.
@@ -32,8 +34,8 @@ class Photo(models.Model):
     tags = models.ManyToManyField(Tag,blank=True)
     persons = models.ManyToManyField(Person, blank=True)
 
-
     def __str__(self):
-        return self.text
+        # É uma boa prática retornar algo que não possa ser nulo, como o ID
+        return f"Photo by {self.owner.username} ({self.id})"
 
 # Create your models here.
